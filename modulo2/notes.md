e3e9c2571fba5a6c70168cc402563258f67a5719
André Lacerda <andrlacerda180@gmail.com>
2021-11-23 15:04:23 -0300

    feat: add coverage and some few tests

--- modulo2/notes.md
+++ modulo2/notes.md
@@ -7,3 +7,8 @@ aí criamos e demos um npm run init-jest, pq npm init --jest não deu certo
 
 
 Instalamos npm i supertest -D
+
+Intalamos npm i faker para gerar dados fictícios
+
+
+No jest.config.js podemos configurar o coverage, que nos ajuda a saber o quanto do código nós já testamos e o que não testamos ainda.user
--- modulo2/project/__tests__/integration/session.test.js
+++ modulo2/project/__tests__/integration/session.test.js
@@ -10,7 +10,9 @@ describe('Authentication', ()  => {
     })
 
     it('should authenticate with valid credentials', async () => {
-        const user = await factory.create('User')
+        const user = await factory.create('User', {
+            password: '123123'
+        })
 
         const response = await request(app)
             .post('/sessions')
@@ -36,7 +38,9 @@ describe('Authentication', ()  => {
     })
 
     it('should return jwt token when authenticated', async () => {
-        const user = await factory.create('User')
+        const user = await factory.create('User', {
+            password: '123123'
+        })
 
         const response = await request(app)
             .post('/sessions')
@@ -46,5 +50,32 @@ describe('Authentication', ()  => {
             })
 
         expect(response.body).toHaveProperty('token')
+    })
+
+    it('should be able to access private routes when athenticated', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer ${user.generateToken()}`)
+
+        expect(response.status).toBe(200)
+    })
+
+    it('should not be able to access private routes without jwt token', async () => {
+        const response = await request(app)
+            .get('/dashboard')
+
+        expect(response.status).toBe(401)
+    })
+
+    it('should not be able to access private routes with invalid jwt token', async () => {
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer 123123`)
+
+        expect(response.status).toBe(401)
     })
 })
